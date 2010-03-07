050d52819c20e5c44cd0d02b25ead3231a57a762
mengu <mengukagan@gmail.com>
2010-03-08 01:23:03 +0200

    Updated actions for file upload

--- actions.py
+++ actions.py
@@ -1,6 +1,7 @@
-from werkzeug import Response, redirect
+# -*- coding: utf-8 -*-
+from werkzeug import Response, redirect, secure_filename
 from utils import render_response
-from models import User, ToDo, session
+from models import User, ToDo, File, session, engine
 import hashlib
 from datetime import datetime
 
@@ -20,24 +21,34 @@ def new_todo(request):
             session.add(new_todo)
             session.commit()
             if new_todo:
-                return redirect("/todo/%s" % new_todo.todoid)
+                sent_file = request.files['userfile']
+                if sent_file:
+                    todo_file = File(todo=new_todo.id, filename=secure_filename(sent_file.filename), filecontent=sent_file.read())
+                    session.add(todo_file)
+                    session.commit()
+                return redirect("/todo/%s" % new_todo.id)
         else:
             return render_response("new_todo.html", user=request.session["user"], error="Please enter a title and note for your to do.")
     else:
         return render_response("new_todo.html", user=request.session["user"])
 
 def view_todo(request, todoid):
-    todo = session.query(ToDo).filter_by(todoid=todoid).first()
+    todo = session.query(ToDo).filter_by(id=todoid).first()
     return render_response("view_todo.html", todo=todo, user=request.session["user"])
 
 def edit_todo(request, todoid):
-    todo = session.query(ToDo).filter_by(todoid=todoid).first()
+    todo = session.query(ToDo).filter_by(id=todoid).first()
     if request.method == 'POST':
         if request.form['title'] and request.form['text']:
             todo.title = request.form['title']
             todo.text = request.form['text']
+            new_file = request.files['userfile']
+            if new_file:
+                existing_file = session.query(File).filter_by(todo_id=todo.id).one()
+                existing_file.filename = secure_filename(new_file.filename)
+                existing_file.filecontent = new_file.read()
             session.commit()
-            return redirect("/todo/%s" % todo.todoid)
+            return redirect("/todo/%s" % todo.id)
         else:
             return render_response("edit_todo.html", todo=todo, user=request.session["user"], error="Please enter a title and note for your to do.")
     else:
@@ -48,6 +59,12 @@ def delete_todo(request, todoid):
     if session.delete(todo):
         session.commit()
         return redirect("/")
+
+def view_file(request, todoid):
+    todo_file = session.query(File).filter_by(todo_id=todoid).one()
+    response = Response(todo_file.filecontent)
+    response.headers['Content-Type'] = 'text/plain'
+    return response
 
 def signup(request):
     if request.method == 'POST':
