9067b44cd1bb0b46303d94bc7d9f4326b4a70647
mengu <mengukagan@gmail.com>
2010-03-08 01:22:33 +0200

    Now using Declarative Base for models

--- models.py
+++ models.py
@@ -1,31 +1,43 @@
-from sqlalchemy import Table, Column, Integer, String, Text, DateTime, MetaData, ForeignKey, create_engine
+# -*- coding: utf-8 -*-
+from sqlalchemy import Table, Column, Integer, String, Text, DateTime, MetaData, ForeignKey, create_engine, BLOB, Unicode
 from sqlalchemy.orm import mapper
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import relation, backref
+from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 from markdown import markdown
 
-metadata = MetaData()
 Session = sessionmaker()
 engine = create_engine("mysql://root:s@localhost/todo")
 Session.configure(bind=engine)
 session = Session()
+Base = declarative_base()
+metadata = Base.metadata
 
-user_table = Table('user', metadata,
-    Column('userid', Integer, primary_key=True),
-    Column('email', String(100)),
-    Column('password', String(32))
-)
+class User(Base):
+    __tablename__ = 'user'
 
-todo_table = Table('todo', metadata,
-    Column('todoid', Integer, primary_key=True),
-    Column('title', String(100)),
-    Column('text', Text),
-    Column('user_id', Integer, ForeignKey('user.userid')),
-    Column('dateline', DateTime)
-)
+    id = Column(Integer, primary_key=True)
+    email = Column(Unicode(150))
+    password = Column(Unicode(32))
 
-class ToDo(object):
+    todos = relation("ToDo", backref="user")
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+class ToDo(Base):
+    __tablename__ = 'todo'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode(150))
+    text = Column(Text)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    dateline = Column(DateTime)
+
+    todofile = relation("File", backref="todo")
+
     def __init__(self, title, text, user):
         self.title = title
         self.text = text
@@ -36,14 +48,16 @@ class ToDo(object):
     def text_html(self):
         return markdown(self.text)
 
-class User(object):
-    def __init__(self, email, password):
-        self.email = email
-        self.password = password
+class File(Base):
+    __tablename__ = 'file'
 
-    def __repr__(self):
-        return "<User %s>" % self.email
+    id = Column(Integer, primary_key=True)
+    todo_id = Column(Integer, ForeignKey('todo.id'))
+    filename = Column(Unicode(100))
+    filecontent = Column(BLOB)
 
-mapper(User, user_table, properties={'todos': relation(ToDo, backref='user')})
-mapper(ToDo, todo_table)
+    def __init__(self, todo, filename, filecontent):
+        self.todo_id = todo
+        self.filename = filename
+        self.filecontent = filecontent
 
